f91630cee0bd5d836bb986e3572f1a04c357fdfc
Angela Strong <ange68s@ucla.edu>
2014-03-13 18:52:33 -0700

    added a TestMain that runs Vitamin D code

---
+++ src/main/TestMain.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package main;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import navigation.server.FastRTParser;
+import navigation.shared.*;
+
+/**
+ *
+ * @author Angela
+ */
+public class TestMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        LatLong start_location = new LatLong(50.5, 4.2);
+        
+        Person person = new Person(4,0);
+        Person person2 = new Person(4,0,50); //(int skinType, int SPF, double bodyExposure)
+
+        person.clothesToBodyExposure("NONE", "SLEEVES-TANK", "PANTS-SHORTS"); //(hat, upper, lower)
+        
+        person.setBodyExposure(60);
+
+
+        VitaminD vitD = new VitaminD();
+
+        // (LatLong, Person, String dateTime)
+        String[] times = vitD.getVitaminDandSunburnTime(start_location, person, "2014-03-19T13:00"); 
+        
+        System.out.println("Vitamin D Time: " + times[0]);
+        System.out.println("Sunburn Time: " + times[1]);
+    }
+    
+}
