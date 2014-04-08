458973caf34542f2673bac1ffb3a88ee0f5b8d13
jried31 <jried31@hotmail.com>
2014-04-08 16:57:17 -0700

    1. Refactored the directory structure 2. Fixed cumulative UV exposure algorithm 3. Fixed issue with building comparison returning deg instead of rad 4. Optimized cach file code 5. Added UV sun/shade statistics

--- src/parse/almonds/DeleteCallback.java
+++
@@ -1,8 +0,0 @@
-package parse.almonds;
-
-import parse.almonds.ParseException;
-
-public abstract class DeleteCallback
-{
-	public abstract void done(ParseException e);	
-}
--- src/parse/almonds/GetCallback.java
+++
@@ -1,6 +0,0 @@
-package parse.almonds;
-
-public abstract class GetCallback
-{
-	public abstract void done(ParseObject object);
-}
